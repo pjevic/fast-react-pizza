f72362ee242dec20e43c323d9b3da05eeb68e50b
Milan Pjevic <milanpjevic@icloud.com>
2024-12-02 22:15:01 +0100

    The Box Model: Spacing, Borders and Display

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Milan</div>;
+  return <div className="hidden text-sm font-semibold">Milan</div>;
 }
 
 export default Username;

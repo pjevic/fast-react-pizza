1e4c63b647eae445ddb6594dc90e7310a1ee5fee
Milan Pjevic <milanpjevic@icloud.com>
2024-12-03 12:22:46 +0100

    Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Milan</div>;
+  return <div className="hidden text-sm font-semibold md:block">Milan</div>;
 }
 
 export default Username;

a68abb66771e31a6cbf556d3d30d913f39bdae49
Milan Pjevic <milanpjevic@icloud.com>
2024-12-03 15:07:24 +0100

    Absolute Positioning and More

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -14,13 +14,11 @@ function AppLayout() {
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-
       <div className="overflow-scroll">
         <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
       </div>
-
       <CartOverview />
     </div>
   );
